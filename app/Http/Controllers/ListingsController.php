f346f90808b4c14a94334a51cf9357d2b8e083b6
Izabel Vidal <izabelvidal99@gmail.com>
2020-07-25 12:54:01 -0300

    first commit

---
+++ app/Http/Controllers/ListingsController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Listing;
+use Illuminate\Support\Facades\Auth;
+
+class ListingsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'address' => 'required',
+            'website' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|integer',
+            'bio' => 'required'   
+        ]);
+
+        $listings = new Listing();
+        $listings->user_id = Auth::id();
+        $listings->name = $request->input('name');
+        $listings->address = $request->input('addres');
+        $listings->website = $request->input('website');
+        $listings->email = $request->input('email');
+        $listings->phone = $request->input('phone');
+        $listings->bio = $request->input('bio');
+        $listings->save();
+
+        return redirect()->to('/home')->with('success', 'Listing created successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Dashboard <span class="float-right"><a href='/listings/create' class="btn btn-secondary">Create listing</a></span></div>
+
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+
+                <h3>Your Listings</h3>
+                @if (count($listings))
+                    <table class= "table table-striped">
+                        <tr>
+                            <th>Company</th>
+                        </tr>
+                        @foreach ($listings as $listings)
+                            <tr>
+                                <td>{{ $listings->name }}</td>
+                            </tr>
+                        @endforeach
+                    </table>
+                @else
+                    <p>You don't have any listing yet</p>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
